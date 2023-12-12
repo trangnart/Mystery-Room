f9870f6490f35c4e044fadd46710357a50745137
Ziyuan Wang <zwang590@ucsc.edu>
2023-12-11 17:39:28 -0800

    Death Message Follows camera

--- Assets/DeathMessageUI.cs
+++ Assets/DeathMessageUI.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class DeathMessageUI : MonoBehaviour
 {
     public Transform playerCamera; // Reference to the player's camera or GameObject
-
+    public float distanceFromCamera = 2f; // Adjust the value as needed
+   
     void Update()
     {
         // Update the Canvas position to be in front of the player's camera
